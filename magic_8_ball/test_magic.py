2df45d44ce0dde1d9002f5df2c73df7075081cd6
MostlyRice <amazinzygy@gmail.com>
2023-01-31 19:25:49 -0600

    Finished Test_magic

--- magic_8_ball/test_magic.py
+++ magic_8_ball/test_magic.py
@@ -1,4 +1,7 @@
-""" TODO create a test case to test each of the following functions,
+""" TODO create a test case to test the following functions,
+
+import functions_magic
+from unittest import TestCase
 
 generate_url_for_question
  - check that the expected URL is returned for an example question. 
@@ -9,9 +12,33 @@ extract_answer_from_response
  - another test should create example dictionaries with a different structure to the one returned from the API, 
  and check that the function returns None. 
 
- 
+
  TODO to think about - what else could you test about this program? 
  What types of expected and unexpected behavior might happen? You may be able to write tests for some 
  of your ideas now. We'll talk about ways to test other aspects of this program in class.
 
 """
+class TestFunctionsMagic(TestCase):
+
+    def test_generate_url_for_question(self):
+        # check that expected URL is returned for an example question
+
+        #  https://8ball.delegator.com/magic/JSON/ 
+        example_question = 'Will I ever love again?'
+        self.assertEqual('https://magic-eight-ball.azurewebsites.net/magic/JSON/Will I ever love again?', functions_magic.generate_url_for_question(example_question))
+
+
+    def test_extract_answer_from_response(self):
+        # create example dictionaries that match expected API response, check that the correct answer is extracted and returned
+
+        magic_dictionary = {
+            'question': 'Will it be sunny tomorrow?', 
+            'answer': 'Yes', 
+            'type': 'Affirmative'
+            }
+
+        self.assertEqual('Yes', functions_magic.extract_answer_from_response(magic_dictionary))
+
+        # create example dictionaries with a different structure to the one returned from the API, check that the function returns None
+        new_structure_dictionary = {}
+
