a79ba47772479eb0c485fc6a6a2929d9e21a7ee5
MostlyRice <amazinzygy@gmail.com>
2023-01-31 18:20:57 -0600

    solved tests for cart discount

--- cart_discount/price_discount.py
+++ cart_discount/price_discount.py
@@ -9,8 +9,18 @@ def discount(item_prices):
     If a customer has ordered more than three items, the cheapest item is free.
     Example: if this function is called with a list of [10, 4, 20] then return 4.
     """
-
-    pass  # todo replace this line with your code 
+    try:
+        number_of_items = len(item_prices)
+        if number_of_items >=3:
+            min_price = min(item_prices)
+            if (min_price < 0):
+                raise ValueError('Price can\'t be negative')
+            return min_price
+        else:     
+            return None
+    except TypeError:
+        return None
+    # pass  # todo replace this line with your code 
 
 
 if __name__ == '__main__':
--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -11,7 +11,35 @@ class TestDiscount(TestCase):
 
     
     # TODO more unit tests here. Each test should test one scenario
-
+    def test_list_of_less_than_three_prices(self):
+        prices = [10,4]
+        expected_discount = None
+        self.assertIsNone(expected_discount, discount(prices))
+    
+    def test_list_of_strings(self):
+        prices = ['item1','item2','item3']
+        expected_discount = None
+        self.assertIsNone(expected_discount, discount(prices))
+    
+    def test_list_of_more_than_three_prices(self):
+        prices = [80, 55, 78, 99, 67]
+        expected_discount = 55
+        self.assertEqual(expected_discount, discount(prices))
+    
+    def test_list_of_mixed_data(self):
+        prices = [35, 'item1', 'apple', 67]
+        expected_discount = None
+        self.assertIsNone(expected_discount, discount(prices))
+    
+    def test_list_if_negative_prices(self):
+        prices = [33, -7, 9, -1, 6]
+        with self.assertRaises(ValueError):
+            discount(prices)
+    
+    def test_if_list_of_same_min_prices(self):
+        prices = [15, 5, 5, 5]
+        expected_discount = 5
+        self.assertEqual(expected_discount, discount(prices))
 
 if __name__ == '__main__':
     unittest.main()
